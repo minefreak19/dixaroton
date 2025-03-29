50d585dad38fa4c022cc35d270c2971a87de184f
minefreak19 <61090340+minefreak19@users.noreply.github.com>
2025-03-30 03:01:57 +0530

    Clean up IntelliJ default comment in `Main.java`

--- src/main/java/me/minefreak19/Main.java
+++ src/main/java/me/minefreak19/Main.java
@@ -8,8 +8,6 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import java.util.EnumSet;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     private static ExarotonClient exarotonClient;
 
